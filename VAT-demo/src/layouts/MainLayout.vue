a473d4d0fb5b2b88222618762baf4b660a1da650
ThanhAnqwe <149473989+ThanhAnqwe@users.noreply.github.com>
2026-07-14 07:44:36 +0000

    base FE structure

---
+++ VAT-demo/src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page-wapper">
+        <PageHeader />
+
+        <!--Filter bar -->
+        <FilterBar/>
+
+        <!--Navigation -->
+        <NavBar/>
+
+        <!--main content-->
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import NavBar from './components/layout/NavBar.vue'
+import FilterBar from './components/layout/FilterBar.vue'
+import PageHeader from './components/layout/PageHeader.vue'
+import ChatBot from './components/layout/ChatBot.vue'
+
+
+
+</script>
+
+<style scoped>
+.page-wapper{
+    position: relative;
+    min-height: 100vh
+}
+</style>
